41a5453e8ba8483391eda9ccbf3ffbc5518f1b70
Brian Hwang <brianhwang29@gmail.com>
2022-04-30 21:05:22 +0900

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+  env: {
+    'vue/setup-compiler-macros': true,
+  },
+};
